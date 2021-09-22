ada5d6fdcc5fc2faf77467e97d419d0286d1e8ac
DarciRoesler <roesler.darci@gmail.com>
2021-09-22 20:57:52 -0300

    Asyncrhronous operations using Tasks (async, await)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,7 +8,7 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
-        
+
         [Required(ErrorMessage = "{0} required")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
@@ -30,9 +30,9 @@ namespace SalesWebMVC.Models
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
